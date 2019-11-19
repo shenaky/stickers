b3c34f74c7dd13fcec33aeeabcdb13600e4d3781
Nao <yasnke@outlook.com>
2019-11-19 19:06:18 +0800

    新增分类接口

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import pymysql
 app = Flask(__name__)
 IMAGE_FOLDER = 'ChineseBQB'
 app.config['IMAGE_FOLDER'] = IMAGE_FOLDER
+app.config['JSON_AS_ASCII'] = False
 basedir = os.path.abspath(os.path.dirname(__file__))
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'gif', 'GIF'])
 
@@ -19,18 +20,54 @@ def hello():
     return "<h1 style='color:blue'>Hello Tsdfhere!</h1>"
 
 @app.route('/category', methods=['GET', 'POST'])
-def get_category():
-    if request.method == 'POST':
-        return '这是一个post请求 c'
+def get_categories():
+    if request.method == 'GET':
+        try:
+            with conn.cursor() as cursor:
+                sql = 'SELECT cid,category FROM categories'
+                cursor.execute(sql)
+                # result = cursor.fetchone()
+                resultall = cursor.fetchall()
+                # fetchmany
+                # print(result)
+                print(resultall)
+            conn.commit()
+        finally:
+            cursor.close()
+        list = []
+        for item in resultall:
+            dict = {}
+            dict['category'] = item[1]
+            dict['cid'] = item[0]
+            list.append(dict)
+        return jsonify({'code' : 0, 'categories': list})
     else:
         return '这是一个get请求'
+
+@app.route('/category/<int:cid>', methods=['GET'])
+def get_category(cid):
+    try:
+        with conn.cursor() as cursor:
+            sql = 'SELECT cid,category FROM categories'
+            cursor.execute(sql)
+            # result = cursor.fetchone()
+            resultall = cursor.fetchall()
+            # fetchmany
+            # print(result)
+            print(resultall)
+        conn.commit()
+    finally:
+        cursor.close()
+    #有的话，就返回列表形式包裹的这个元素，没有的话就报错404   
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+    #否则，将这个task以json的格式返回。
 
 @app.route('/collection', methods=['GET', 'POST'])
 def get_collection():
     if request.method == 'POST':
-        return '这是一个post请求 co'
-    else:
-        return '这是一个get请求 co'
+        pass
 
 @app.route('/make', methods=['GET', 'POST'])
 def get_make():
