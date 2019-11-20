ad9672f78143b5169d0c6a3f6c26a1102b29a833
Nao <yasnke@outlook.com>
2019-11-20 20:11:08 +0800

    新增主页接口

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from flask import Flask,request,jsonify,send_from_directory,make_response
+from random import shuffle
 import json
 import os
 import pymysql
@@ -18,6 +19,41 @@ cursor =conn.cursor()
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello Tsdfhere!</h1>"
+
+'''
+/**
+* showdoc
+* @catalog 接口
+* @title 获取表情包主页
+* @description 获取表情包主页的接口
+* @method get
+* @url http://111.230.153.254/api/home
+* @return {"code":0, "data":[{"sid":1,"url":"http://111.230.153.254/large/052Squirtle_%E6%9D%B0%E5%B0%BC%E9%BE%9F/Squirtle22.JPG"},{"cid":"1","url":"http://111.230.153.254/large/052Squirtle_%E6%9D%B0%E5%B0%BC%E9%BE%9F/Squirtle22.JPG"}]}
+* @return_param code int 状态
+* @return_param sid int 表情id
+* @return_param url string url
+*/
+'''
+
+@app.route('/api/home', methods=['GET'])
+def get_home():
+    try:
+        with conn.cursor() as cursor:
+            sql = 'SELECT sid,url FROM stickers'
+            cursor.execute(sql)
+            resultall = cursor.fetchall()
+            # print(resultall)
+        conn.commit()
+    finally:
+        cursor.close()
+    list = []
+    for item in resultall:
+        dict = {}
+        dict['sid'] = item[0]
+        dict['url'] = item[1]
+        list.append(dict)
+    shuffle(list)
+    return jsonify({'code' : 0, 'data': list})
 
 '''
 /**
