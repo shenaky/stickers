9608dcba8743709e98c1816fbb4527407510c267
Nao <yasnke@outlook.com>
2019-11-22 15:05:56 +0800

    修改分类接口

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from flask import Flask,request,jsonify,send_from_directory,make_response
 from random import shuffle
 import json
 import os
+import requests
 import pymysql
+from auth_token import create_token,verify_token,login_required
 
 app = Flask(__name__)
 IMAGE_FOLDER = 'ChineseBQB'
@@ -19,6 +21,61 @@ cursor =conn.cursor()
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello Tsdfhere!</h1>"
+
+@app.route("/api/login",methods=["POST"])
+def login():
+    '''
+    用户登录
+    :return:token
+    '''
+    res_dir = request.get_json()
+    if res_dir is None:
+        #这里的code，依然推荐用一个文件管理状态
+        return jsonify(code = 4103,msg = "未接收到参数")
+    
+    #获取前端传过来的参数
+    appid = res_dir.get("code")
+    secret = res_dir.get("secret")
+    js_code = res_dir.get("js_code")
+    
+    #校验参数
+    url = 'https://api.weixin.qq.com/sns/jscode2session'
+    querystring = {
+            "appid": appid,
+            "secret": secret,
+            "js_code": js_code,
+            "grant_type": "authorization_code"
+        }
+    r = requests.get(url, params=querystring)
+    errcode = r.json()['errcode']
+    try:
+        openid = r.json()['openid']
+    except:
+        print
+    
+    try:
+        with conn.cursor() as cursor:
+            sql = 'SELECT uid FROM user WHERE openid = %s'
+            cursor.execute(sql, (openid))
+            result = cursor.fetchone()
+        conn.commit()
+    finally:
+        cursor.close()
+    if not result:
+        try:
+            cursor.execute("INSERT INTO user (openid) VALUES (%s)", (openid))
+            cursor.connection.commit()
+        except:
+            print("sql insert error")
+            conn.rollback()
+        finally:
+            cursor.close()
+    
+    #创建token
+    token = create_token(openid)
+
+    #把token返回给前端
+    return jsonify(code=0,msg="succeed",data=token)
 
 '''
 /**
@@ -68,6 +125,8 @@ def get_home():
 * @return_param code int 状态
 * @return_param cid int 分类id
 * @return_param category string 分类名称
+* @return_param sid int 表情id
+* @return_param url string 表情url 
 */
 '''
 
@@ -76,7 +135,10 @@ def get_categories():
     if request.method == 'GET':
         try:
             with conn.cursor() as cursor:
-                sql = 'SELECT cid,category FROM categories'
+                sql = '''SELECT categories.cid,categories.category,any_value(stickers.sid),any_value(url)
+                        FROM stickers,belong,categories
+                        WHERE stickers.sid = belong.sid AND belong.cid = categories.cid
+                        GROUP BY cid,category'''
                 cursor.execute(sql)
                 resultall = cursor.fetchall()
                 print(resultall)
@@ -88,6 +150,8 @@ def get_categories():
             dict = {}
             dict['category'] = item[1]
             dict['cid'] = item[0]
+            dict['sid'] = item[2]
+            dict['url'] = item[3]
             list.append(dict)
         return jsonify({'code' : 0, 'data': list})
     else:
@@ -101,6 +165,8 @@ def get_categories():
 * @description 获取一类表情包的url的接口
 * @method get
 * @url http://111.230.153.254/api/category/<int:cid>
+* @param limit 必选 int 每页条数
+* @param page 必选 int 页数
 * @return {"code":0,"cid" : 12, "data":[{"sid":1,"url":"http://111.230.153.254/large/052Squirtle_%E6%9D%B0%E5%B0%BC%E9%BE%9F/Squirtle22.JPG"},{"cid":"1","url":"http://111.230.153.254/large/052Squirtle_%E6%9D%B0%E5%B0%BC%E9%BE%9F/Squirtle22.JPG"}]}
 * @return_param code int 状态
 * @return_param cid int 分类id
@@ -111,12 +177,14 @@ def get_categories():
 
 @app.route('/api/category/<int:cid>', methods=['GET'])
 def get_category(cid):
+    limit = int(request.args.get('limit'))
+    page = int(request.args.get('page'))
+    offset = limit * page
     try:
         with conn.cursor() as cursor:
-            sql = 'SELECT sid,url FROM stickers WHERE sid IN (SELECT sid FROM belong WHERE cid = %s)'
-            cursor.execute(sql, (cid))
+            sql = 'SELECT sid,url FROM stickers WHERE sid IN (SELECT sid FROM belong WHERE cid = %s) ORDER BY sid DESC LIMIT %s OFFSET %s'
+            cursor.execute(sql, (cid, limit, offset))
             resultall = cursor.fetchall()
-            print(resultall)
         conn.commit()
     finally:
         cursor.close()
